3813caf0649f88002fdaf213b951ed4f00d19660
Levi Bernard <levichristianbernard@gmail.com>
2020-07-28 09:07:57 -0500

    completed fibonacci sequence

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  if (n < 1) {
+    return 'OOPS'
+  } else if (n <= 2) {
+    return 1
+  } else {
+    let sequence = [1, 1]
+    for (let i = 2; i < n; i++) {
+      sequence[i] = sequence[i - 1] + sequence[i - 2]
+    }
+    return sequence[n - 1]
+  }
 }
 
 module.exports = fibonacci
