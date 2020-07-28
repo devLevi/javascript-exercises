3627d66bfbd7512aaaf4b2401f7a74ecf947e216
Levi Bernard <levichristianbernard@gmail.com>
2020-07-28 08:41:55 -0500

    complete calc excersizes

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,32 +1,47 @@
-function add () {
-	
+function add(x, y) {
+  return x + y
 }
 
-function subtract () {
-	
+function subtract(x, y) {
+  return x - y
 }
 
-function sum () {
-	
+function sum(x) {
+  if (x.length === 0) return 0
+  else {
+    let sum = x.reduce(function (a, b) {
+      return a + b
+    })
+    return sum
+  }
 }
 
-function multiply () {
-	
+function multiply(array) {
+  return array.length ? array.reduce((acc, nextItem) => acc * nextItem) : 0
 }
 
-function power() {
-	
+function power(x, y) {
+  return Math.pow(x, y)
 }
 
-function factorial() {
-	
+function factorial(n) {
+  let answer = 1
+  if (n == 0 || n == 1) {
+    return answer
+  } else {
+    for (let i = n; i >= 1; i--) {
+      answer = answer * i
+    }
+    return answer
+  }
+  //   return n ? n * factorial(n - 1) : 1
 }
 
 module.exports = {
-	add,
-	subtract,
-	sum,
-	multiply,
-    power,
-	factorial
+  add,
+  subtract,
+  sum,
+  multiply,
+  power,
+  factorial,
 }
